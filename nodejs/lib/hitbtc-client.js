8b7aa6a81fada2c71824840acd6c9ba8bc10d48b
agbares <Antonio.G.Bares@gmail.com>
2018-05-07 14:14:27 -0700

    Fixed an issue where writeNewCurrencyDataToFile() was constantly calling the callback function

--- nodejs/lib/hitbtc-client.js
+++ nodejs/lib/hitbtc-client.js
@@ -148,15 +148,16 @@ exports.writeNewCurrencyDataToFile = function(filename, _callback) {
         {
             data.forEach(function(tickerData, symbol) {
 
-                console.log(tickerData.get('quoteCurrency'));
-                console.log(tickerData.get('baseCurrency'));
-                console.log(tickerData.get('last'));
+                // console.log(tickerData.get('quoteCurrency'));
+                // console.log(tickerData.get('baseCurrency'));
+                // console.log(tickerData.get('last'));
 
-                console.log('\n');
+                // console.log('\n');
                 let dataToWrite = `${tickerData.get('quoteCurrency')},${tickerData.get('baseCurrency')},${tickerData.get('last')}\n`;
 
                 fs.appendFile(filename, dataToWrite, function(err) {
-                    _callback(err);
+                    if(err)
+                        _callback(err);
                 })
             });
 
